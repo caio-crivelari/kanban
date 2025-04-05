8984122a9b3511c1c354cabff1534dc13cd57d7f
caio-crivelari <caio_crivelari@hotmail.com>
2025-04-05 17:21:40 -0300

    Added drag and drop feature

--- js/main.js
+++ js/main.js
@@ -1,20 +1,69 @@
-const cardsDescription = document.querySelectorAll('.card p');
+const cardsDescription = document.querySelectorAll(".card p");
 const characterLimit = 67;
 
-cardsDescription.forEach(paragraph => {
-    const fullText = paragraph.innerText; // Guarda o texto original
-    const isAboveLimit = fullText.length > characterLimit;
-    
-    if (isAboveLimit) {
-        paragraph.dataset.fullText = fullText; // Armazena o texto completo no atributo data
-        paragraph.innerText = fullText.substring(0, characterLimit) + '...';
+cardsDescription.forEach((paragraph) => {
+  const fullText = paragraph.innerText; // Guarda o texto original
+  const isAboveLimit = fullText.length > characterLimit;
 
-        paragraph.addEventListener('mouseenter', () => {
-            paragraph.innerText = fullText; // Mostra o texto completo ao passar o mouse
-        });
+  if (isAboveLimit) {
+    paragraph.dataset.fullText = fullText; // Armazena o texto completo no atributo data
+    paragraph.innerText = fullText.substring(0, characterLimit) + "...";
 
-        paragraph.addEventListener('mouseleave', () => {
-            paragraph.innerText = fullText.substring(0, characterLimit) + '...'; // Volta ao encurtado ao tirar o mouse
-        });
-    }
+    paragraph.addEventListener("mouseenter", () => {
+      paragraph.innerText = fullText; // Mostra o texto completo ao passar o mouse
+    });
+
+    paragraph.addEventListener("mouseleave", () => {
+      paragraph.innerText = fullText.substring(0, characterLimit) + "..."; // Volta ao encurtado ao tirar o mouse
+    });
+  }
 });
+
+/* DRAG AND DROP */
+
+const cardItem = document.querySelectorAll(".card");
+
+cardItem.forEach((card) => {
+  card.addEventListener("dragstart", dragStart);
+  card.addEventListener("drag", drag);
+  card.addEventListener("dragend", dragEnd);
+});
+
+function dragStart(e) {
+  dropzones.forEach((dropzone) => {
+    dropzone.classList.add("highlighted-zone");
+  });
+  e.currentTarget.classList.add("is-dragging"); // Adiciona a classe "dragging" ao item arrastado
+}
+
+function drag() {}
+
+function dragEnd(e) {
+  dropzones.forEach((dropzone) => {
+    dropzone.classList.remove("highlighted-zone");
+  });
+  e.currentTarget.classList.remove("is-dragging");
+}
+
+const dropzones = document.querySelectorAll(".dropzone");
+
+dropzones.forEach((dropzone) => {
+  dropzone.addEventListener("dragenter", dragenter);
+  dropzone.addEventListener("dragover", dragover);
+  dropzone.addEventListener("dragleave", dragleave);
+  dropzone.addEventListener("drop", drop);
+});
+
+function dragenter() {}
+
+function dragover(e) {
+  e.currentTarget.classList.add("over");
+  const draggingCard = document.querySelector(".is-dragging");
+  e.currentTarget.appendChild(draggingCard);
+}
+
+function dragleave(e) {
+  e.currentTarget.classList.remove("over");
+}
+
+function drop() {}
